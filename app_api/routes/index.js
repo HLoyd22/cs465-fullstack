793a3ec8d2af05631c84b3339ac88532e2605b90
FatSleepyPanda <fatsleepypanda22@gmail.com>
2023-04-16 16:21:14 -0700

    Express website, static HTML only

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,7 +5,13 @@ const tripsController = require("../controllers/trips");
 
 router
   .route("/trips")
-  .get(tripsController.tripsList);
+  .get(tripsController.tripsList)
+  .post(tripsController.tripsAddTrip);
+
+router
+  .route("/trips/:tripCode")
+  .get(tripsController.tripsList)
+  .put(tripsController.tripsUpdateTrip);
 
 router
   .route("/trips/:tripCode")
